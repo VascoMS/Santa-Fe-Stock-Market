156dcc998cc82f0db60c4f0d2d2be67777f1d375
VascoMS <vasco2002silva@gmail.com>
2025-05-15 20:39:16 +0100

    (feat) Added slope to price updates

--- src/market_maker.py
+++ src/market_maker.py
@@ -2,17 +2,17 @@ import numpy as np
 from typing import Dict, Tuple
 
 class Asset:
-    def __init__(self, initial_dividend: float, ro: float, alpha: float, bank_return: float, supply: int):
+    def __init__(self, initial_dividend: float, rho: float, alpha: float, bank_return: float, supply: int):
         self._dividend = initial_dividend
         self._dividend_mean = initial_dividend
         self._prev_dividend_delta = self._dividend - self._dividend_mean
         self._price = initial_dividend / bank_return # setting initial price to fair price of d(t)/r 
-        self._ro = ro
-        self._alpha = alpha
+        self._ro = rho # recommended -> 0.9 for d = 2 r = 0.02
+        self._alpha = alpha # recommended -> 0.15 for d = 2 r = 0.02
         self._supply = supply
 
     def update_dividend(self) -> float:
-        delta = self._ro * self._prev_dividend_delta + self._alpha * np.random.normal(loc=0, scale=1)
+        delta = self._rho * self._prev_dividend_delta + self._alpha * np.random.normal(loc=0, scale=1)
         self._prev_dividend_delta = delta
         self._dividend = self._dividend_mean + delta
         return self._dividend
@@ -49,16 +49,17 @@ class Auction:
         self._price = asset.get_price()
         self._supply = asset.get_supply()
         self._demand = 0
+        self._slope = 0
         self._k = k
     
-    def add_demand(self, amount: int):
+    def add_demand(self, amount: int, slope: int):
         self._demand += amount
+        self._slope += slope
     
     def determine_price(self) -> Tuple[int, bool]:
         delta = self._demand - self._supply
-        cleared = delta == 0
+        cleared = delta < 1e-2
         if not cleared:
-            self._price = self._price + self._k * delta
+            self._price = self._price - self._k * (delta/self._slope)
             self._demand = 0
         return self._price, cleared
-    
