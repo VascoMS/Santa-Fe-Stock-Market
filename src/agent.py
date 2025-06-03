Fixed price history updates, added alternative dividend calculation, added hree mode

src/agent.py
@@ -4,12 +4,10 @@ import numpy as np
 from constants import *
 from predictor import Predictor
 from typing import Any, Dict, List, Tuple
-import random
 
 class Agent:
     def __init__(self, id: str, cash: float):
         self._id = id
-        random.random()
         # holdings in each of the three assets
         self._portfolio = {"asset_1": 0, "asset_2": 0, "asset_3": 0}
         # cash on hand
@@ -20,6 +18,7 @@ class Agent:
         self._latest_observation = None
 
         self._activated_predictors: Dict[str, List[Predictor]] = {"asset_1": [], "asset_2": [], "asset_3": []}
+        self._previous_activated_predictors: Dict[str, List[Predictor]] = {"asset_1": [], "asset_2": [], "asset_3": []}
 
         # create a pool of predictors per asset
         self._predictors: Dict[str, List[Predictor]] = {}
@@ -91,18 +90,24 @@ class Agent:
                 RISK_AVERSION * self._latest_predictor.get_variance()
             )
             qty = self._bound_demand(np.round(target_h, 2), self._portfolio[asset], price)
-            #print(f"Agent {self._id} - Asset: {asset}, Expected: {self._expected}, Price: {price}, Target_h: ({self._expected} - {price} * (1 + {INTEREST_RATE})) / ({RISK_AVERSION} * {self._latest_predictor.get_variance()}) = {target_h}, Demand: {qty}, a: {self._latest_predictor.get_parameter_a()}, b: {self._latest_predictor.get_parameter_b()}")
 
             # Record for portfolio update
             self._demand[asset] = qty
             # Submit to auction 
             slope = (self._latest_predictor.get_parameter_a() - (1 + INTEREST_RATE)) / (RISK_AVERSION * self._latest_predictor.get_variance()) # dh/dp
-            #print(f"Agent {self._id} - Asset: {asset}, Slope: {slope}")
+            print(f"Agent {self._id} - Asset: {asset}, Expected: {self._expected}, Price: {price}, Demand: {qty}, Slope: {slope} a: {self._latest_predictor.get_parameter_a()}, b: {self._latest_predictor.get_parameter_b()} variance: {self._latest_predictor.get_variance()} bitstring: {self._latest_predictor._condition_string}")
             demands_and_slope[asset] = (qty, slope)
             
         return demands_and_slope
     
     def _bound_demand(self, demand, current_holding, price) -> None:
+        delta = demand - current_holding
+        if delta > 10:
+            # If demand is too high, cap it to prevent excessive buying
+            demand = current_holding + 10
+        elif delta < -10:
+            # If demand is too low, cap it to prevent excessive selling
+            demand = current_holding - 10
         return min(max(demand, 0), self._cash/price + current_holding)
     
     def compute_wealth(self) -> float:
@@ -160,11 +165,15 @@ class Agent:
                 true_price = prices[asset]
                 true_dividend = dividends[asset]
                 
-                for predictor in self._activated_predictors[asset]:
+                for predictor in self._previous_activated_predictors[asset]:
                     predictor.update(true_price, true_dividend)
+                
+                # Update the previous activated predictors
+                self._previous_activated_predictors[asset] = self._activated_predictors[asset]
+                self._activated_predictors[asset] = []
                     
                 # Occasionally evolve predictors
-                if random.random() < (1.0 / GENETIC_EXPLORATION_PARAMETER):
+                if np.random.rand() < (1.0 / GENETIC_EXPLORATION_PARAMETER) and MODE != 1:
                     self._evolve_predictors(asset)
     
     def update(self):
@@ -207,7 +216,7 @@ class Agent:
             child._b = parent_choice.get_parameter_b()
         
         for i in range(len(parent_1._condition_string)):
-            if random.random() < 0.5:
+            if np.random.rand() < 0.5:
                 child._condition_string[i] = parent_1._condition_string[i]
             else:
                 child._condition_string[i] = parent_2._condition_string[i]
@@ -234,18 +243,18 @@ class Agent:
         eligible_parents = sorted_predictors[:-num_to_replace]
 
         for predictor in worst:
-            if random.random() <= CROSSOVER_RATE:
+            if np.random.rand() <= CROSSOVER_RATE:
                 new_predictor = self.crossover(
                     self.get_parent_for_evolution(asset, eligible_parents),
                     self.get_parent_for_evolution(asset, eligible_parents),
                 )
             else:
                 new_predictor = self.get_parent_for_evolution(asset, eligible_parents).clone()
-            if random.random() < 0.03:
+            if np.random.rand() < 0.03:
                 new_predictor.mutate_params()
             bit_mutated = False
             for i in range(len(new_predictor._condition_string)):
-                if random.random() < 0.03:
+                if np.random.rand() < 0.03:
                     new_predictor._condition_string[i] = np.random.choice(['0', '1', '#'])
                     bit_mutated = True
             if bit_mutated:
@@ -253,8 +262,4 @@ class Agent:
                 new_predictor._variance = mean_variance
             self._predictors[asset].remove(predictor)
             self._predictors[asset].append(new_predictor)
-
-
-        
-        # Swap out the worst predictor with the new one
         

src/market_maker.py
@@ -2,23 +2,21 @@ from collections import deque
 import numpy as np
 from typing import Dict, Tuple, List
 from constants import *
+from math import sqrt
 
 class Asset:
-    def __init__(self, initial_dividend: float, rho: float, alpha: float, supply: int):
+    def __init__(self, initial_dividend: float, rho: float, supply: int):
         self._dividend = initial_dividend
         self._dividend_mean = initial_dividend
         self._prev_dividend_delta = self._dividend - self._dividend_mean
         self._price = initial_dividend / INTEREST_RATE * 0.8
         self._price_history = deque([self._price]) 
         self._rho = rho # recommended -> 0.9 for d = 2 r = 0.02
-        self._alpha = alpha # recommended -> 0.15 for d = 2 r = 0.02
         self._supply = supply
 
 
     def update_dividend(self) -> float:
-        delta = self._rho * self._prev_dividend_delta + self._alpha * np.random.normal(loc=0, scale=1)
-        self._prev_dividend_delta = delta
-        self._dividend = self._dividend_mean + delta
+        self._dividend = self._dividend_mean + self._rho*(self._dividend - self._dividend_mean) + np.random.normal(0, sqrt(ASSET_1_ERROR_VARIANCE))
         return self._dividend
     
     def get_dividend(self) -> float:
@@ -49,16 +47,16 @@ class Auction:
     def add_demand(self, amount: int, slope: int):
         self._demand += amount
         self._slope += slope
-        
     
     def determine_price(self) -> int:
         delta = self._demand - self._supply
-        self._cleared = abs(delta) <= 0.1
+        self._cleared = abs(delta) <= 0.01
         if not self._cleared:
-            if abs(self._slope) < 1e-3:
-                self._slope = -1e-3 if self._slope < 0 else 1e-3
-            self._price = max(0.01, self._price - self._k * (delta/self._slope))
-            #print(f"Ran auction : Price = {self._price}, Demand = {self._demand}, Supply = {self._supply}, Slope = {self._slope}, Delta = {delta}")
+            if self._slope != 0:
+                self._price = min(max(0.01, self._price - self._k * (delta/self._slope)), 500)
+            else:
+                self._price = min(max(0.01, self._price + self._k*delta), 500)
+            print(f"Ran auction : Price = {self._price}, Demand = {self._demand}, Supply = {self._supply}, Slope = {self._slope}, Demand diff = {delta}")
             self._demand = 0
             self._slope = 0
         return self._price
@@ -67,7 +65,7 @@ class Auction:
         return self._cleared
 
 class MarketMaker:
-    K = 0.01
+    K = 0.1
 
     def __init__(self):
         self._assets: Dict[str, Asset] = self._create_assets()
@@ -78,7 +76,6 @@ class MarketMaker:
         "asset_1": Asset(
             initial_dividend=ASSET_1_INITIAL_DIVIDEND,
             rho=ASSET_1_RHO,
-            alpha=ASSET_1_ALPHA,
             supply=ASSET_1_SUPPLY
         ),
         # "asset_2": Asset(
@@ -114,7 +111,8 @@ class MarketMaker:
             auction = self._auctions[asset_id]
             if auction:
                 asset.set_price(auction._price)
-                #print(f"Auction cleared for {asset_id}: Price = {auction._price}, Demand = {auction._demand}, Supply = {auction._supply}")
+                asset
+                print(f"Auction cleared: Price = {auction._price}, Demand = {auction._demand}, Supply = {auction._supply}")
             
     def start_auctions(self):
         for asset_id, asset in self._assets.items():

src/predictor.py
@@ -1,12 +1,12 @@
 import random
 import numpy as np
-from constants import NUM_INDICATORS, LAMBDA, M, C
+from constants import *
 
 class Predictor:
     def __init__(self, asset_name: str):
         self._asset_name = asset_name
-        self._a = np.random.uniform(0.7, 1.2)
-        self._b = np.random.uniform(-10, 19.002)
+        self._a = np.random.uniform(0.7, 1.2) if MODE != 1 else HREE_A
+        self._b = np.random.uniform(-10, 19.002) if MODE != 1 else HREE_B
         self._variance = 4.0
         self._last_prediction = None
         self._condition_string = [np.random.choice(['0', '1', '#'], p=[0.1, 0.1, 0.8]) for _ in range(NUM_INDICATORS)]